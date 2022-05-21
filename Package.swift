0dc1fa34358ede4abf21b130a6a226adee5b5694
Shane Dickson <sdickson9740@me.com>
2022-05-21 11:38:10 -0400

    updates

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["DKImagePickerController"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/zhangao0086/DKCamera", .branch("develop")),
+        .package(url: "https://github.com/dicksonconsultingllc/DKImagePickerController", .branch("develop")),
         .package(url: "https://github.com/zrn-ns/DKPhotoGallery", .branch("feature/support_swiftpm")),
         .package(url: "https://github.com/TimOliver/TOCropViewController", from: "2.6.0"),
     ],
